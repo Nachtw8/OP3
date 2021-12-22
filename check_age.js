f9b60cf19db951e4dd2c46a9761600202c01172f
Nachtw8 <94112184+Nachtw8@users.noreply.github.com>
2021-12-22 23:18:37 +0100

    Add files via upload

---
+++ check_age.js
@@ -0,0 +1,23 @@
+let age = 16;
+let isFemale = false;
+let driverStatus = 'bob';
+
+if (age > 18) {
+    console.log('The Person is allowed in');
+}  else {
+    console.log ('The person is too young and is kindly requested to stay outside');
+}
+
+/* Check if the person is a woman */
+
+if (isFemale) {
+    console.log ('The Person is a woman!');
+} else {
+    console.log ('The person is a man!');
+}
+
+if (driverStatus == 'bob') {
+    console.log ('The driver is the Bob!')
+} else {
+    console.log ('The person is not a Bob!')
+}
